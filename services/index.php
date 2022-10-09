02abe8a2d6a5372dfa064834ab28d4c3a7cdc4b4
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-10-09 16:47:01 +0400

    separate service page

---
+++ services/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Services</title>
+
+    <link rel="stylesheet" href="./styles/style.css" />
+    <!--services styles--->
+    <link rel="stylesheet" href="./styles/services.css">
+</head>
+
+<body>
+    <div class="main_wrapper">
+        <div class="main_container">
+            <!----navigation container------>
+            <!----navigation container------>
+            <!----navigation container------>
+            <div class="navigation__container">
+                <div class="navigation__container_wrapper">
+                    <!---photo logo--->
+                    <div class="navigation__logo">
+                        <a href="#">
+                            <div class="navigation__img">
+                                <img src="./styles/img/logo.png" alt="" />
+                            </div>
+                            <div class="navigation__text">Victoria Lipnitskaya
+                            </div>
+                            <p>Photographer</p>
+                        </a>
+                    </div>
+                    <!------------>
+                    <!---navigation navigation---->
+                    <div class="navigation__nav_container">
+                        <div class="navigation__elements">
+                            <!--ссылка---->
+                            <div class="navigation__element">
+                                <a href="./index.html">Home</a>
+                            </div>
+                            <!--ссылка---->
+                            <div class="navigation__element">
+                                <a href="./about.html">About</a>
+                            </div>
+                            <!--ссылка---->
+                            <div class="navigation__element">
+                                <a href="./services.html">Services</a>
+                            </div>
+                            <!--ссылка---->
+                            <div class="navigation__element">
+                                <a href="./gallery.html">Gallery</a>
+                            </div>
+                            <!--ссылка---->
+                            <div class="navigation__element">
+                                <a href="#">Feedback</a>
+                            </div>
+                            <!--ссылка---->
+                            <div class="navigation__element">
+                                <a href="./blog.html">Blog</a>
+                            </div>
+                            <!--ссылка---->
+                            <div class="navigation__element">
+                                <a href="./contact.html">Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-------->
+                    <!--Navigation info-->
+                    <div class="navigation__social_container">
+                        <div class="navigation__social__wrapper">
+                            <div class="navigation__social_icons">
+                                <a href="#">
+                                    <img src="./styles/img/insta.png" alt="" />
+                                </a>
+                                <a href="#">
+                                    <img src="./styles/img/fb.png" alt="" />
+                                </a>
+                                <a href="#">
+                                    <img src="./styles/img/yt.png" alt="" />
+                                </a>
+                                <a href="#">
+                                    <img src="./styles/img/fb.png" alt="" />
+                                </a>
+                            </div>
+                            <div class="navigation__social_desc">
+                                <p>© 2022 Lipnitskaya by DAKER</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!----content container----->
+            <!----content container----->
+            <!----content container----->
+            <div class="content__container">
+                <div class="content__wrapper">
+
+                    <!---services php container--->
+
+                    <!--main page footer---->
+                    <div class="footer__block">
+                        <div class="footer__block__container">
+                            <div class="footer__desc">
+
+                                <h3>Victoria Lipnitskaya</h3>
+                                <p>PHOTOGRAPHER</p>
+                            </div>
+                            <div class="footer__rights">
+                                © 2022 Lipnitskaya by DAKER
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
